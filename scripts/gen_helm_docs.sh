a2020f1ff2ce90bab6af851e236c455b399fa65b
Jose Ramon Mañes <jose@celestia.org>
2024-07-05 14:15:34 +0200

    ref: cleanup folder
    Signed-off-by: Jose Ramon Mañes <jose@celestia.org>

--- scripts/gen_helm_docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in $(ls charts|grep celestia); do
-    cd charts/$i
-    echo "Generating docs for chart: [$i]"
-    docker run --rm --volume "$(pwd):/helm-docs" -u $(id -u) jnorwood/helm-docs:latest
-    # go to git repo root
-    cd $(git rev-parse --show-toplevel)
-done
