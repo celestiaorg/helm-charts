3fa6fcbe3a504d1c47ab5da1d30e92945bb90f35
Smuu <18609909+Smuu@users.noreply.github.com>
2024-09-27 08:41:09 +0200

    feat: bump node to v0.18.1
    Signed-off-by: Smuu <18609909+Smuu@users.noreply.github.com>

--- scripts/extract_node_config.sh
+++ scripts/extract_node_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check if the correct number of arguments are provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <celestia-app-version> <celestia-node-version>"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <celestia-node-version>"
     exit 1
 fi
 
@@ -13,36 +13,12 @@ if ! command -v docker &> /dev/null; then
 fi
 
 # Assign arguments to variables
-CELESTIA_APP_VERSION=$1
-CELESTIA_NODE_VERSION=$2
+CELESTIA_NODE_VERSION=$1
 
 # Generate unique container names
-APP_CONTAINER_NAME="celestia-app-container-$(date +%s)"
 BRIDGE_CONTAINER_NAME="celestia-bridge-container-$(date +%s)"
 FULL_CONTAINER_NAME="celestia-full-container-$(date +%s)"
 LIGHT_CONTAINER_NAME="celestia-light-container-$(date +%s)"
-
-# Start the celestia-app container with the specified version and keep it running
-docker run -d --name $APP_CONTAINER_NAME --entrypoint tail ghcr.io/celestiaorg/celestia-app:$CELESTIA_APP_VERSION -f /dev/null
-
-# Execute the command in the celestia-app container
-docker exec $APP_CONTAINER_NAME celestia-appd init app --chain-id celestia
-
-# Create a directory named as the version in the scripts/config/app folder
-SCRIPT_DIR=$(dirname "$0")
-APP_CONFIG_DIR="$SCRIPT_DIR/config/app/$CELESTIA_APP_VERSION"
-mkdir -p $APP_CONFIG_DIR
-
-# Copy the config files from the celestia-app container to the folder
-docker cp $APP_CONTAINER_NAME:/home/celestia/.celestia-app/config/app.toml $APP_CONFIG_DIR
-docker cp $APP_CONTAINER_NAME:/home/celestia/.celestia-app/config/client.toml $APP_CONFIG_DIR
-docker cp $APP_CONTAINER_NAME:/home/celestia/.celestia-app/config/config.toml $APP_CONFIG_DIR
-
-# Stop and remove the celestia-app container in the background
-docker stop $APP_CONTAINER_NAME
-docker rm $APP_CONTAINER_NAME
-
-echo "Config files for celestia-app version $CELESTIA_APP_VERSION have been extracted to $APP_CONFIG_DIR"
 
 # Function to extract config files for a given node type
 extract_node_configs() {
@@ -56,6 +32,7 @@ extract_node_configs() {
     docker exec $CONTAINER_NAME celestia $NODE_TYPE init
 
     # Create a directory named as the version in the scripts/config/node/<node_type> folder
+    SCRIPT_DIR=$(dirname "$0")
     NODE_CONFIG_DIR="$SCRIPT_DIR/config/node/$NODE_TYPE/$CELESTIA_NODE_VERSION"
     mkdir -p $NODE_CONFIG_DIR
 
