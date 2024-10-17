2eb327e90c3beb2117c1beea268a033a6db1c95e
MΞTΔ <19519604+aWN4Y25pa2EK@users.noreply.github.com>
2024-10-17 14:33:49 +0100

    Migrate to `common-celestia-node` (#63)
    * migrate to common-celestia-node
    
    * fix docs version for celestia-node and fix make generate-docs script path
    
    * version the helm-docs generator to avoid relying on the latest release

--- scripts/gen_helm_docs.sh
+++ scripts/gen_helm_docs.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-for i in $(ls charts|grep celestia); do
+# Go to the git root directory before listing charts
+cd $(git rev-parse --show-toplevel)
+
+# Loop through the charts directory
+for i in $(ls charts | grep celestia); do
     cd charts/$i
     echo "Generating docs for chart: [$i]"
-    docker run --rm --volume "$(pwd):/helm-docs" -u $(id -u) jnorwood/helm-docs:latest
-    # go to git repo root
+    docker run --rm --volume "$(pwd):/helm-docs" -u $(id -u) jnorwood/helm-docs:v1.14.2
+
+    # Go back to the git repo root after each chart generation
     cd $(git rev-parse --show-toplevel)
 done
