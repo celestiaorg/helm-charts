7991ccf67bae2b8257369538deb0971a944c4430
Pippo <pippokr@protonmail.com>
2025-05-15 09:28:43 +0100

    created a new version (#122)

--- charts/celestia-app/files/setup.sh
+++ charts/celestia-app/files/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# CELESTIA_HOME should be set in the environment
+# CELESTIA_APP_HOME should be set in the environment
 # APP_ID should be set in the environment
 # CHAIN_ID should be set in the environment
 # INITIAL_TIA_AMOUNT should be set in the environment
@@ -11,30 +11,30 @@ KEY_NAME="${APP_ID}-${CHAIN_ID}"
 echo "---------------------------------------------------"
 echo "Checking if the file [genesis.json] exists or not..."
 echo "---------------------------------------------------"
-if [[ ! -f "$CELESTIA_HOME/config/genesis.json" ]]; then
-    celestia-appd init "${APP_ID}" --home "${CELESTIA_HOME}" --chain-id "${CHAIN_ID}"
+if [[ ! -f "$CELESTIA_APP_HOME/config/genesis.json" ]]; then
+    celestia-appd init "${APP_ID}" --home "${CELESTIA_APP_HOME}" --chain-id "${CHAIN_ID}"
 
-   # yes n | celestia-appd keys add "${KEY_NAME}" --home "${CELESTIA_HOME}" --no-backup
-    celestia-appd keys add "${KEY_NAME}" --home "${CELESTIA_HOME}" --no-backup
+   # yes n | celestia-appd keys add "${KEY_NAME}" --home "${CELESTIA_APP_HOME}" --no-backup
+    celestia-appd keys add "${KEY_NAME}" --home "${CELESTIA_APP_HOME}" --no-backup
 
-    account_address=$(celestia-appd keys show "${KEY_NAME}" -a --home "${CELESTIA_HOME}")
-    celestia-appd add-genesis-account "${account_address}" "${INITIAL_TIA_AMOUNT}" --home "${CELESTIA_HOME}"
+    account_address=$(celestia-appd keys show "${KEY_NAME}" -a --home "${CELESTIA_APP_HOME}")
+    celestia-appd add-genesis-account "${account_address}" "${INITIAL_TIA_AMOUNT}" --home "${CELESTIA_APP_HOME}"
 
-    celestia-appd gentx "${KEY_NAME}" "${STAKING_TIA_AMOUNT}" --home "${CELESTIA_HOME}" --chain-id "${CHAIN_ID}"
+    celestia-appd gentx "${KEY_NAME}" "${STAKING_TIA_AMOUNT}" --home "${CELESTIA_APP_HOME}" --chain-id "${CHAIN_ID}"
 
-    celestia-appd collect-gentxs --home "${CELESTIA_HOME}"
+    celestia-appd collect-gentxs --home "${CELESTIA_APP_HOME}"
 
     echo "Copying the keys to the config path..."
-    mv $CELESTIA_HOME/config/priv_validator_key.json $CELESTIA_HOME/keys
-    mv $CELESTIA_HOME/config/node_key.json $CELESTIA_HOME/keys
+    mv $CELESTIA_APP_HOME/config/priv_validator_key.json $CELESTIA_APP_HOME/keys
+    mv $CELESTIA_APP_HOME/config/node_key.json $CELESTIA_APP_HOME/keys
 fi
 
-echo "Configuration applied [$CELESTIA_HOME/config/config.toml]"
+echo "Configuration applied [$CELESTIA_APP_HOME/config/config.toml]"
 echo "---------------------------------------------------"
-cat $CELESTIA_HOME/config/config.toml
-echo "Configuration applied [$CELESTIA_HOME/config/app.toml]"
+cat $CELESTIA_APP_HOME/config/config.toml
+echo "Configuration applied [$CELESTIA_APP_HOME/config/app.toml]"
 echo "---------------------------------------------------"
-cat $CELESTIA_HOME/config/app.toml
-echo "Configuration applied [$CELESTIA_HOME/config/client.toml]"
+cat $CELESTIA_APP_HOME/config/app.toml
+echo "Configuration applied [$CELESTIA_APP_HOME/config/client.toml]"
 echo "---------------------------------------------------"
-cat $CELESTIA_HOME/config/client.toml
+cat $CELESTIA_APP_HOME/config/client.toml
